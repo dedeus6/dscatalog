eebdcf5a4ea30450aa39a1d90572e5931ef99bda
Filipe de Deus <filipedeus2015@gmail.com>
2021-09-13 15:37:34 -0300

    Find Category by id & exception handler

--- backend/src/main/java/com/bootcamp/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/bootcamp/dscatalog/services/CategoryService.java
@@ -1,13 +1,17 @@
 package com.bootcamp.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bootcamp.dscatalog.dto.CategoryDTO;
 import com.bootcamp.dscatalog.entities.Category;
 import com.bootcamp.dscatalog.repositories.CategoryRepository;
+import com.bootcamp.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,8 +20,18 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return categoryRepository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = categoryRepository.findAll();
+		
+		return list.stream().map(item -> new CategoryDTO(item)).collect(Collectors.toList());
+
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 
 }
